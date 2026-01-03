c08c8d0da66c099043e1029073d92f80e138c3f0
Nikodem <n.golawski@gmail.com>
2026-01-03 22:00:58 +0100

    final touches

--- Semestr 3/Algorytmika/projekt/DaneWejsciowe.cpp
+++ Semestr 3/Algorytmika/projekt/DaneWejsciowe.cpp
@@ -4,72 +4,74 @@
 
 using namespace std;
 
-static const vector<vector<Macierz>> DANE = {
+static const vector<vector<Macierz> > DANE = {
 
     // METODA 1
     {
-            {   // zestaw 1
-                { 2,  1, -1,  8},
-                {-3, -1,  2, -11},
-                {-2,  1,  2, -3}
-            },
-            {   // zestaw 2
-                { 0,  2,  9, 7},
-                { 1, -1,  2, 3},
-                { 3,  2, -1, 5}
-            }
+        {
+            // zestaw 1
+            {2, 1, -1, 8},
+            {-3, -1, 2, -11},
+            {-2, 1, 2, -3}
+        },
+        {
+            // zestaw 2
+            {0, 2, 9, 7},
+            {1, -1, 2, 3},
+            {3, 2, -1, 5}
+        }
     },
 
     // METODA 2
     {
-            {
-                {0.0001, 1, 1, 2},
-                {1,      2, 3, 6},
-                {2,      1, 1, 4}
-            },
-            {
-                {1e-20, 1, 1, 2},
-                {1,     1, 2, 4},
-                {2,     2, 3, 6}
-            }
+        {
+            {0.0001, 1, 1, 2},
+            {1, 2, 3, 6},
+            {2, 1, 1, 4}
+        },
+        {
+            {1e-20, 1, 1, 2},
+            {1e-20, 1, 1, 2},
+            {1, 2, 3, 6}
+        }
     },
 
     // METODA 3
     {
-            {
-                { 1,  2,  3, 6},
-                { 4,  5,  6, 15},
-                { 7,  8, 10, 25}
-            },
-            {
-                { 1, 1000, 1, 1002},
-                { 2, 1,    1, 4},
-                { 3, 1,    1, 5}
-            }
+        {
+            {1, 2, 3, 6},
+            {4, 5, 6, 15},
+            {7, 8, 10, 25}
+        },
+        {
+            {1, 1000, 1, 1002},
+            {2, 1, 1, 4},
+            {3, 1, 1, 5}
+        }
     },
 
     // METODA 4
     {
-            {
-                { 1, 2, 3, 6},
-                { 4, 5, 6, 15},
-                { 7, 8, 10, 25}
-            },
-            {
-                { 0, 2, 3, 5},
-                { 1, 0, 4, 6},
-                { 2, 1, 0, 3}
-            }
+        {
+            {1, 2, 3, 6},
+            {4, 5, 6, 15},
+            {7, 8, 10, 25}
+        },
+        {
+            {0, 2, 3, 5},
+            {1, 0, 4, 6},
+            {2, 1, 0, 3}
+        }
     }
 };
 
-const Macierz& DaneWejsciowe::pobierz(int metoda, int zestaw) {
+const Macierz &DaneWejsciowe::pobierz(int metoda, int zestaw) {
     return DANE.at(metoda - 1).at(zestaw - 1);
 }
 
 
 void DaneWejsciowe::pokaz(int metoda, int zestaw) {
-    const Macierz& macierz = pobierz(metoda, zestaw);
+    const Macierz &macierz = pobierz(metoda, zestaw);
 
     for (size_t i = 0; i < macierz.size(); i++) {
         for (size_t j = 0; j < macierz[i].size() - 1; j++) {
@@ -79,9 +81,9 @@ void DaneWejsciowe::pokaz(int metoda, int zestaw) {
     }
 }
 
-void DaneWejsciowe::pokaz(const Macierz& macierz) {
+void DaneWejsciowe::pokaz(const Macierz &macierz) {
     cout << "-------------------------\n";
-    for (const auto& wiersz : macierz) {
+    for (const auto &wiersz: macierz) {
         for (size_t j = 0; j < wiersz.size() - 1; j++) {
             cout << wiersz[j] << " ";
         }
@@ -89,6 +91,3 @@ void DaneWejsciowe::pokaz(const Macierz& macierz) {
     }
     cout << "-------------------------\n";
 }
-
-
-
